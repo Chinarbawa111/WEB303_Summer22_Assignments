56d6b0aaaadeb165073853a919ee1d1c3891dda2
Chinarbawa111 <84715612+Chinarbawa111@users.noreply.github.com>
2022-05-26 17:35:24 -0400

    script.js edited
    Edit the existing script.js file

--- WEB303S22As01/script.js
+++ WEB303S22As01/script.js
@@ -1,11 +1,11 @@
 /*
 	WEB 303 Assignment 1 - jQuery
-	ChinaR bAWA
+	Chinar Bawa
 */
 $(document).ready(function(){
-$("#price").keyup(function(){
-	let insertValue = $("#price").val();
-	let calTax = insertValue*0.13;
-	$("#tax").text("$"+ calTax.toFixed(2));
-})
-})
+	$("#price").keyup(function(){
+		let insertValue = $("#price").val();
+		let calTax = insertValue*0.13;
+		$("#tax").text("$"+ calTax.toFixed(2));
+	})
+	})
